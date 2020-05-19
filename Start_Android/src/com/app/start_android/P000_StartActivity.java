c3777faedcec225d3acadf1678b9fc84badc45fb
Pavel PahiDD <pahidd@outlook.com>
2020-05-19 15:01:43 +0700

    P005_LayoutFiles

--- Start_Android/src/com/app/start_android/P000_StartActivity.java
+++ Start_Android/src/com/app/start_android/P000_StartActivity.java
@@ -13,7 +13,7 @@ import android.widget.TextView;
 
 public class P000_StartActivity extends Activity implements View.OnClickListener
 	{
-		Button btnp004;
+		Button btnp004,btnp005;
 
 		@Override
 		protected void onCreate(Bundle savedInstanceState)
@@ -40,11 +40,18 @@ public class P000_StartActivity extends Activity implements View.OnClickListener
 				btnp004.setId(R.id.btn_p004);
 				btnp004.setOnClickListener(this);
 				btnp004.setLayoutParams(rowParams);
+				
+				btnp005 = new Button(this);
+				btnp005.setText(R.string.title_p005);
+				btnp005.setId(R.id.btn_p005);
+				btnp005.setOnClickListener(this);
+				btnp005.setLayoutParams(rowParams);
 
 				TableRow tableRow_04 = new TableRow(this);
 				tableRow_04.setLayoutParams(tableParams);
 				tableLayout.addView(tableRow_04);
 				tableRow_04.addView(btnp004);
+				tableRow_04.addView(btnp005);
 
 			}
 
@@ -56,6 +63,10 @@ public class P000_StartActivity extends Activity implements View.OnClickListener
 					{
 					case R.id.btn_p004:
 						intent = new Intent(this, P004_BasicViews.class);
+						startActivity(intent);
+						break;
+					case R.id.btn_p005:
+						intent = new Intent(this, P005_LayoutFiles.class);
 						startActivity(intent);
 						break;
 					default:
---
+++ Start_Android/src/com/app/start_android/P005_LayoutFiles.java
@@ -0,0 +1,15 @@
+package com.app.start_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class P005_LayoutFiles extends Activity
+	{
+
+		@Override
+		protected void onCreate(Bundle savedInstanceState)
+			{
+				super.onCreate(savedInstanceState);
+				setContentView(R.layout.myscreen);
+			}
+	}
